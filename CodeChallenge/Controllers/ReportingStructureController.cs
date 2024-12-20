89ea63e3e8ce23d75c757ab17fa1686d6151ebb7
Nick Warriner <nwarriner@magicwrighter.com>
2024-12-20 18:00:05 -0500

    Remove unused usings

--- CodeChallenge/Controllers/ReportingStructureController.cs
+++ CodeChallenge/Controllers/ReportingStructureController.cs
@@ -1,11 +1,8 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CodeChallenge.Services;
-using CodeChallenge.Models;
 
 namespace CodeChallenge.Controllers
 {
--- CodeChallenge/Services/ICompensationService.cs
+++ CodeChallenge/Services/ICompensationService.cs
@@ -1,7 +1,5 @@
 using CodeChallenge.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using OneOf;
 using OneOf.Types;
